bbaf68aa6a6c22d51581c7387870e9e971143fd6
student-apnaschoole <mukkarramk1@gmail.com>
2025-07-29 19:12:05 +0300

    Add files via upload

---
+++ strings/script.js
@@ -0,0 +1,100 @@
+//string
+// let str="apnaschoole";
+//let str2="mukkarram";
+//console.log(str[8]);
+
+
+
+// let obj={
+//     item:"pen",
+//     price: 10,
+// }
+// let output=`the cost of ${obj.item} is${obj.price} rupees`;
+// console.log(output);
+
+// console.log("the cost of",obj.item,"is",obj.price,"rupees");
+
+//template literals
+// let specialstring= `this is template literal ${1+2+3}`;
+// console.log( specialstring);
+// console.log("apna \n collage");
+// console.log("apna \t collage");
+// let str="apna collage";
+// console.log(str.length);
+
+
+// let str="apnacollage";
+// // let newstr=str.toUpperCase();
+// str=str.toUpperCase();
+// console.log(str);
+// // console.log(newStr);
+
+
+
+// let str="apnacollage";
+// // let newstr=str.tolowerCase();
+// str=str.tolowerCase();
+// console.log(str);
+// // console.log(newStr);
+ 
+// let str="        apan collage js    ";
+// console.log(str.trim());   
+
+
+// let str="hello";
+// console.log(str.slice(0,5));
+
+// let str1="apna";
+// let str2="collage";
+// let res ="I am learing coding from" + str1 + str2;
+// let res="hello"+123;
+//  let res = str1.concat(str2);
+// console.log(res);
+
+// let str="hellppp";
+
+// // console.log(str.replace("lo","p"));
+// console.log(str.replaceAll ("lo","p"))
+
+
+let str="I lovejs";
+
+console.log(str.charAt(3));//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
